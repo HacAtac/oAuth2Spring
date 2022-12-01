62b607a1a357812bada5d0366c802a10b6e148ff
Jordan Hackworth <jhackworth00@outlook.com>
2022-12-01 17:53:18 -0600

    registered Auth convertor class with HttpSecurity

--- ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class WebSecurity  {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        //Registered (Authentication convertor class) with HttpSecurity
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
         http
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/api/v1/users/status/check")
@@ -19,7 +25,8 @@ public class WebSecurity  {
                 .anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer()
-                .jwt();
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
         return http.build();
     }
 }
