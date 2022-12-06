ccb36cae7f9678477e04c780cbe7d046c3a41244
Jordan Hackworth <jhackworth00@outlook.com>
2022-12-06 12:08:06 -0600

    New Project and Register an OAuth Client with spring auth server

---
+++ NewSpringAuthorizationServer/src/main/java/com/appsdeveloperblog/photoapp/oauthserver/AuthorizationServerConfiguration.java
@@ -0,0 +1,37 @@
+package com.appsdeveloperblog.photoapp.oauthserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+
+import java.util.UUID;
+
+@Configuration
+public class AuthorizationServerConfiguration {
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client1")
+                .clientSecret("{noop}myClientSecretValue") //since giving a clientSecret value this means that it wil be a private client
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/users-client-oidc")
+                .redirectUri("http://127.0.0.1:8080/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope("read")
+                //below commented is if you want to use a consent page || or can do .requireProofKey(true) which will force a proof of key
+                //.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+}
